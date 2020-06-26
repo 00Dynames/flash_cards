5c863fe4837fc9d8e924e1ccd6a9e54a5320b266
dbunadi <dbunadi@gmail.com>
2020-06-26 17:29:16 +1000

    add handler to serve 10 random phrases

--- server/main.go
+++ server/main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main(){
+type Card struct {
+	SideA string `json: "sideA"`
+	SideB string `json: "sideB"`
+}
 
-	fmt.Println("hjello")
+func getPhrases(w http.ResponseWriter, r *http.Request) {
+
+	cards := []Card{}
+
+	database, err := sql.Open("sqlite3", "../data/flash_card_data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// TODO: get random rows in a more efficient way
+	rows, err := database.Query("select jpn_text, jpn_transcription || '-' || eng_text from jpn_eng_phrases order by random() limit 10")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		c := Card{}
+		err = rows.Scan(&c.SideA, &c.SideB)
+		if err != nil {
+			log.Fatal(err)
+		}
+		cards = append(cards, c)
+	}
+
+	fmt.Println(cards)
+
+	json.NewEncoder(w).Encode(cards)
+	database.Close()
+}
+
+func main() {
+
 	router := mux.NewRouter()
-
 	// Return 10 random phrase pairs
-	router.HandleFunc("/api/1.0/phrases", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello")
-	}).Methods("GET")
+	router.HandleFunc("/api/1.0/phrases", getPhrases).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
